e58d0f5b94fcd9127106c98d3f10a3faedf707c0
hydrartew <artyambs@mail.ru>
2025-10-07 14:38:42 +0300

    commit changes

--- kafka_app/broker.py
+++ kafka_app/broker.py
@@ -8,7 +8,4 @@ from config_reader import settings
 logger = logging.getLogger(__name__)
 
 
-# Shared Kafka broker instance configured from settings
 broker = KafkaBroker(settings.KAFKA_BROKER_URL)
-
-
